c7a5accff9e748a814a000b9270f162adca0cf46
ben-jamin-chan <chanbenjamin.tl@gmail.com>
2025-06-09 18:23:29 +0800

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"; // ✅ Import this
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename="/lynn-florist-website"> {/* ✅ Add this */}
       <App />
